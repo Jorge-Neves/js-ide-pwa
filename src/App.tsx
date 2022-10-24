201bb734c54d7267553be625e979e54f48fbd1de
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-10-24 17:06:25 +0100

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <>
+      <iframe src="iframe/editor.html" title="editor" className="editor" />
+    </>
+  );
+};
+
+export default App;
